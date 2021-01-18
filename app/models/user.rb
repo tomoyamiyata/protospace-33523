3f9a8ec6ce8f1fa8c07e37a367665032c3b1864d
tomoyamiyata <tomoya.0310mdesu@gmail.com>
2021-01-18 16:16:26 +0900

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
-  validates :position, presence: true         
+  validates :position, presence: true   
+  
+  has_many :prototypes
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
+
+  resources :prototypes, only: [:new, :create, :new, :show, :edit, :update, :destroy] do
+  end
+
+  resources :prototype do
+    resources :comments, only: :create
+  end
+
+  resources :users, only: :show
 end
