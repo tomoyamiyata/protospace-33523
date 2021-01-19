b8acb46fff5dd828e28c4231e0f921ea1742a6fa
tomoyamiyata <tomoya.0310mdesu@gmail.com>
2021-01-19 14:16:17 +0900

    henkou

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :authenticate_user!, except: [:index, :show, ]
+  before_action :move_to_index, except: [:index, :show]
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -28,6 +28,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to root_path
+    end
     
   end
   
